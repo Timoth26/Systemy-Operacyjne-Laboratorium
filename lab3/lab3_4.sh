67c2af9b19e314f2846140082bf46cafbdcf14be
tomek <254040@student.pwr.edu.pl>
2023-04-24 13:30:49 +0200

    add lab3_4.sh

---
+++ lab3/lab3_4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+# standardowe wyjscie przekieruj do nicosci, a bledy posortuj (nie usuwaj duplikatow)
+./fakaping.sh > /dev/null 2>&1 | tee sort
+
+#  wszystkie errory zawierajace "permission denied" wyppisz na konsole i do pliku
+./fakaping.sh 2>&1 | grep -i "permission denied" | sort -u | tee denied.log
+
+# wszystkie unikalne linijki zapisz do pliku all.log i na konsole
+./fakaping.sh 2>&1 | sort -u | tee all.log
