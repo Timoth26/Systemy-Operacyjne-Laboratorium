cad6fc1485964b2ac2e9c4d4cb8d6761fded3c24
Timoth26 <254040@student.pwr.edu.pl>
2023-05-29 14:11:21 +0200

    added __name__

--- lab5/main.py
+++ lab5/main.py
@@ -2,11 +2,14 @@ from studentA import print_board, is_game_over, new_board
 from studentB import ai_move, get_user_move, is_player_starting
 from student import annouce_outcome
 
-board=new_board()
-players_move=is_player_starting()
-while not is_game_over(board):
-    print_board(board)
-    board=players_move and get_user_move(board) or ai_move(board)
-    players_move =not players_move
 
-annouce_outcome(board,players_move)
+if __name__ == '__main__':
+
+    board = new_board()
+    players_move = is_player_starting()
+    while not is_game_over(board):
+        print_board(board)
+        board = players_move and get_user_move(board) or ai_move(board)
+        players_move = not players_move
+
+    annouce_outcome(board, players_move)
