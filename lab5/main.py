4090d9bc50bdb1d920c9fcfe2d6c72de656ebeee
Timoth26 <254040@student.pwr.edu.pl>
2023-05-29 14:12:02 +0200

    Imports cleaned up

--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,5 @@
 from studentA import print_board, is_game_over, new_board
-from studentB import ai_move, get_user_move, is_player_starting
-from student import annouce_outcome
+from studentB import ai_move, get_user_move, is_player_starting, announce_outcome
 
 
 if __name__ == '__main__':
